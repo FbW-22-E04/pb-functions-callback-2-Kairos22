904015d368bc992509d15879a429aa293f3591f5
Kairos22 <alandiko84@gmail.com>
2022-08-23 13:57:28 +0200

    square

---
+++ index.js
@@ -0,0 +1,10 @@
+// Number Square
+const arr = [1, 2, 3, 4, 5, 6]
+
+arraysquare = arr.map(function (a) {
+    return a * a
+});
+
+console.log(arraysquare);
+console.log('#####################');
+
